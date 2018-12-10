17619ed5f1be1d2487d69e461e05e217ca0b74d1
Anthony Faraco-Hadlock <fh.anthony@gmail.com>
2018-12-10 09:15:53 -0800

    Saving current state

--- src/set2/challenge13/13challenge.go
+++ src/set2/challenge13/13challenge.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	ca "cryptoattackers"
 	ch "cryptohelpers"
+	"errors"
 	"fmt"
 	"strings"
 )
@@ -64,6 +65,10 @@ func parseProfile(profile string) (map[string]string, error) {
 func profileFor(email string) (string, error) {
 	profile := make(map[string]string)
 
+	if strings.Contains(email, "&") || strings.Contains(email, "=") {
+		return "", errors.New("Invalid character")
+	}
+
 	profile["email"] = email
 	profile["id"] = "10"
 	profile["role"] = "user"
@@ -82,16 +87,24 @@ func profileFor(email string) (string, error) {
 
 	result = strings.TrimSuffix(result, "&")
 
+	// TODO remove
+	fmt.Printf("profileString: %v\n", result)
+
 	return result, nil
 }
 
-func getEncryptedProfile(profile string) ([]byte, error) {
+func getEncryptedProfile(email string) ([]byte, error) {
 	// Generate a random key if needed
 	if superSecretKey == nil {
 		fmt.Println("GENERATED KEY")
 		keyByteCount := 16
 		superSecretKey = make([]byte, keyByteCount)
 		rand.Read(superSecretKey)
+	}
+
+	profile, err := profileFor(email)
+	if err != nil {
+		panic(err)
 	}
 
 	cipher, err := ch.EncryptAESECB(superSecretKey, []byte(profile))
@@ -102,8 +115,26 @@ func getEncryptedProfile(profile string) ([]byte, error) {
 	return cipher, nil
 }
 
+func getDecryptedProfile(cipher []byte) (map[string]string, error) {
+	// The key should already be generated
+	if superSecretKey == nil {
+		return nil, errors.New("No decryption key")
+	}
+
+	profileBytes, err := ch.DecryptAESECB(superSecretKey, cipher)
+	if err != nil {
+		panic(err)
+	}
+
+	profile, err := parseProfile(string(profileBytes))
+	if err != nil {
+		panic(err)
+	}
+
+	return profile, nil
+}
+
 func main() {
-	// TODO undefined: err (say what?!)
 	blockSizeBytes, err := ca.DetermineBlockSize(func(input []byte) ([]byte, error) { return getEncryptedProfile(string(input)) })
 	if err != nil {
 		panic("couldn't determine block size")
@@ -111,10 +142,82 @@ func main() {
 
 	fmt.Printf("blockSizeBytes: %v\n", blockSizeBytes)
 
-	cipher, err := getEncryptedProfile("testemail@mydomain.com")
+	// We know that the string we pass will get the following added before: "email="
+	// And the following added after: "&uid=10&role=user"
+	// We want some cipher that has "user" by itself in a block so that we can
+	// paste on top of that.
+	prependSize := 6 // length of "email="
+	appendSize := 12 // length of "&id=10&role="
+	//userSize := 4    // length of "user"
+
+	pasteEmailSize := (blockSizeBytes - prependSize - appendSize) % blockSizeBytes // the email we'll use for the final "attack" profile
+	if pasteEmailSize < 0 {
+		pasteEmailSize += blockSizeBytes
+	}
+
+	copyPrependSize := (blockSizeBytes - prependSize) % blockSizeBytes // the begining email size we'll use to get our "admin" + padding block
+	if copyPrependSize < 0 {
+		copyPrependSize += blockSizeBytes
+	}
+
+	fmt.Printf("pasteEmailSize %v\n", pasteEmailSize)
+	fmt.Printf("copyPrependSize %v\n", copyPrependSize)
+
+	// Construct the attack email
+	var attackBuilder strings.Builder
+	attackSuffix := "@b.com"
+	if pasteEmailSize < len(attackSuffix)+1 {
+		panic("EmailSizeIsTooSmall")
+	}
+	for i := 0; i < pasteEmailSize-len(attackSuffix); i++ {
+		attackBuilder.WriteString("a")
+	}
+	attackBuilder.WriteString(attackSuffix)
+	fmt.Printf("Length of attackString: %v\n", len(attackBuilder.String()))
+
+	// The last block should contain "user" + padding
+	// We'll paste in "admin" + padding later
+	cipher, err := getEncryptedProfile(attackBuilder.String())
 	if err != nil {
 		panic("getEncryptedProfile error")
 	}
 
 	fmt.Printf("cipher: %v\n", cipher)
+
+	profile, err := getDecryptedProfile(cipher)
+
+	fmt.Printf("profile: %v\n", profile)
+
+	// Construct the "email" to get our admin block that will be copied
+	var copyBuilder strings.Builder
+	for i := 0; i < copyPrependSize; i++ {
+		copyBuilder.WriteString("a")
+	}
+	adminString := "admin"
+	copyBuilder.WriteString(adminString)
+	for i := 0; i < blockSizeBytes-len(adminString); i++ {
+		copyBuilder.WriteString(string(byte(blockSizeBytes - len(adminString))))
+	}
+	fmt.Printf("Length of copyString: %v\n", len(copyBuilder.String()))
+
+	copyCipher, err := getEncryptedProfile(copyBuilder.String())
+	if err != nil {
+		panic("getEncryptedProfile error")
+	}
+
+	fmt.Printf("copyCipher: %v\n", copyCipher)
+
+	copyProfile, err := getDecryptedProfile(copyCipher)
+
+	fmt.Printf("copyProfile: %v\n", copyProfile)
+
+	// Copy the bytes over
+	for i := 0; i < blockSizeBytes; i++ {
+		cipher[len(cipher)-blockSizeBytes+i] = copyCipher[blockSizeBytes+i]
+	}
+
+	finalProfile, err := getDecryptedProfile(cipher)
+
+	fmt.Printf("finalProfile: %v\n", finalProfile)
+
 }
